eebf5fcae3f6eac5aecf1e5c607ad89c466cb219
smithbwlhs <smithb@wlwv.k12.or.us>
2024-10-15 15:09:59 -0700

    update

--- src/math_class/Main.java
+++ src/math_class/Main.java
@@ -4,6 +4,7 @@ package math_class;
  * https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/lang/Math.html
  */
 
+import java.math.MathContext;
 import java.util.Random;
 
 public class Main {
@@ -16,23 +17,51 @@ public class Main {
         // ClassName.methodName(); e.g. Math.random()
         // ClassName.staticVariableName(); e.g. Math.PI
 
+        double myPI = Math.PI;
+
         // abs
+        int num1 = -2;
+        int absValue = Math.abs(num1);
+        System.out.println(absValue);
 
         // pow
+        double num2 = Math.pow(3,5);
+        System.out.println(num2); //cast to int if you want that
 
         // sqrt
-
-
+        double num3 = Math.sqrt(80);
+        System.out.println(num3);
+        System.out.println(Math.pow(num3,2)); //round off error
 
         //create a random number in the range [0,1)
+        double random1 = Math.random();
+        System.out.println(random1);
 
-        //create a random number in the range [0, n)
+        //create a random integer in the range [0, n)
+        // example: [0,6)
+        double random2 = (int)(Math.random()*6);
+        // in general, multiply by n
 
-        // create a random number in the range [0,n]
+        // create a random  in the range [0,n]
+        // example [0,6]
+        double random3 = (int)(Math.random()*7);
 
-        // create a random number in the range [m,n]
+
+        //For Homework (you can make up m and n values)
+
+        // 1) create a random integer in the range [m,n]
+
+        // 2) create a random negative integer in the range [-m,0]
+
+        // 3) create a random negative integer in the range [-m,-n]
+
+        // 4) create a random negative integer in the range [-m,n]
 
 
+
+
+
+        // Save for Thursday
 
         //using the Random Class
         //This is not static so we have to create a Random object
